5a81ea940466c5425a572422eee099c8ee9def5e
EvgenPEO <zheka12125@gmail.com>
2023-05-09 17:21:33 +0300

    l3 v3

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include "Automata.h"
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-	Automata coffeeMachine;
-	coffeeMachine.turnOn();
-	coffeeMachine.displayMenu();
-	coffeeMachine.insertCoin(30);
-	coffeeMachine.selectItem();
-	coffeeMachine.verifyPayment();
-	coffeeMachine.prepareItem();
-	coffeeMachine.turnOff();
-	return 0;
-} 
+    setlocale(LC_ALL, "Russian");
+    Automata coffeeMachine;
+    coffeeMachine.turnOn();
+    coffeeMachine.displayMenu();
+    coffeeMachine.insertCoin(30);
+    coffeeMachine.selectItem();
+    coffeeMachine.verifyPayment();
+    coffeeMachine.prepareItem();
+    coffeeMachine.turnOff();
+    return 0;
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -3,27 +3,27 @@
 #include "Automata.h"
 
 TEST(task1, test1) {
-	Automata coffeeMachine;
-	coffeeMachine.turnOn();
-	coffeeMachine.insertCoin(50);
-	State currentState = coffeeMachine.getState();
-	EXPECT_EQ(ACCEPT, currentState);
+    Automata coffeeMachine;
+    coffeeMachine.turnOn();
+    coffeeMachine.insertCoin(50);
+    State currentState = coffeeMachine.getState();
+    EXPECT_EQ(ACCEPT, currentState);
 }
 
 TEST(task2, test2) {
-	Automata coffeeMachine;
-	coffeeMachine.turnOn();
-	coffeeMachine.insertCoin(50);
-	int currentCash = coffeeMachine.getCash();
-	EXPECT_EQ(50, currentCash);
+    Automata coffeeMachine;
+    coffeeMachine.turnOn();
+    coffeeMachine.insertCoin(50);
+    int currentCash = coffeeMachine.getCash();
+    EXPECT_EQ(50, currentCash);
 }
 
 TEST(task3, test3) {
-	Automata coffeeMachine;
-	coffeeMachine.turnOn();
-	coffeeMachine.insertCoin(50);
-	coffeeMachine.selectItem();
-	coffeeMachine.verifyPayment();
-	int currentCash = coffeeMachine.getCash();
-	EXPECT_EQ(30, currentCash);
-} 
+    Automata coffeeMachine;
+    coffeeMachine.turnOn();
+    coffeeMachine.insertCoin(50);
+    coffeeMachine.selectItem();
+    coffeeMachine.verifyPayment();
+    int currentCash = coffeeMachine.getCash();
+    EXPECT_EQ(30, currentCash);
+}
